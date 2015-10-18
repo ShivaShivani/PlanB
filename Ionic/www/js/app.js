f9a10677a79ae8aaeb5ed9d7cf99f2e1a989fb17
ShivaniGaddala <akhilagaddala@akhilas-mbp.kc.umkc.edu>
2015-10-18 17:20:33 -0500

    event page updated

--- Ionic/www/js/app.js
+++ Ionic/www/js/app.js
@@ -5,34 +5,35 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('PlanB', ['ionic', 'PlanB.controller'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-}).config(function ($stateProvider,$urlRouterProvider){
-    $stateProvider
-      .state('login',{
-        url: '/login',
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .state('register', {
-        url: '/register',
-        templateUrl: 'views/register.html',
-        controller: 'RegisterCtrl'
-      })
-      .state('home',{
-        url: '/home',
-        templateUrl: 'views/home.html'
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    }).config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: 'views/register.html',
+                controller: 'RegisterCtrl'
+            })
+            .state('home', {
+                url: '/home',
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl'
 
-      });
-    $urlRouterProvider.otherwise('/login');
+            });
+        $urlRouterProvider.otherwise('/login');
 
-  });
+    });
--- Ionic/www/js/controller.js
+++ Ionic/www/js/controller.js
@@ -3,42 +3,77 @@
  */
 
 angular.module('PlanB.controller', ['ionic', 'ui.router'])
-  .controller('LoginCtrl', function ($scope, $state, $http, $httpParamSerializerJQLike) {
-    $scope.pageClass = 'login';
-    $scope.register = function () {
-      $state.go('register')
-    }
-    $scope.login = function (username, password) {
-    }
-  })
-  .controller('RegisterCtrl', function ($scope, $state, $http, $httpParamSerializerJQLike) {
-    $scope.home = function () {
-      $state.go('home')
-    }
-    $scope.pageClass = 'register';
-    $scope.register = function (username, password, confirmpassword, email) {
-      console.log("inside login function");
-      if (password == confirmpassword) {
-        $http({
-          method: 'POST',
-          url: 'https://api.mongolab.com/api/1/databases/asedemo/collections/users?apiKey=Xx4htLKN3YDE1zVeTjZBZJJxn4AyJZ74',
-          data: JSON.stringify({
-            name: username,
-            password: password,
-            email: email
-          }),
-          contentType: "application/json"
-        }).success(function () {
-          $scope.userName = "";
-          $scope.password = "";
-          $scope.email = "";
+    .controller('LoginCtrl', function ($scope, $state, $http, $httpParamSerializerJQLike) {
+        $scope.pageClass = 'login';
+        $scope.register = function () {
+            $state.go('register')
+        }
+        $scope.login = function (username, password) {
+            $state.go('home')
+        }
 
-          //$scope.msg2 ="User created successfully";
-        })
-      } else {
-        $scope.msg1 = "Password did not match";
-      }
-    }
+    })
+    .controller('RegisterCtrl', function ($scope, $state, $http, $httpParamSerializerJQLike) {
+        $scope.home = function () {
+            $state.go('home')
+        }
+        $scope.pageClass = 'register';
+        $scope.register = function (username, password, confirmpassword, email) {
+            console.log("inside login function");
+            if (password == confirmpassword) {
+                $http({
+                    method: 'POST',
+                    url: 'https://api.mongolab.com/api/1/databases/asedemo/collections/users?apiKey=Xx4htLKN3YDE1zVeTjZBZJJxn4AyJZ74',
+                    data: JSON.stringify({
+                        name: username,
+                        password: password,
+                        email: email
+                    }),
+                    contentType: "application/json"
+                }).success(function () {
+                    $scope.userName = "";
+                    $scope.password = "";
+                    $scope.email = "";
 
-  })
+                    //$scope.msg2 ="User created successfully";
+                })
+            } else {
+                $scope.msg1 = "Password did not match";
+            }
+        }
+
+    })
+    .controller('HomeCtrl', function ($scope, $state, $ionicModal) {
+        $scope.events = [];
+
+        //open our plan modal
+        $scope.plan = function () {
+            $scope.eventModal.show();
+        };
+        //Create and load the Modal
+        $ionicModal.fromTemplateUrl('views/plan.html', function (modal) {
+            $scope.eventModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+        //called when event is submitted
+        $scope.create = function (event) {
+            $scope.events.push({
+                title: event.title,
+                location: event.location,
+                datetime: event.datetime
+            });
+            $scope.closeEvent();
+
+            event.title = "";
+            event.location = "";
+            event.datetime = "";
+        };
+        // Close the event modal
+        $scope.closeEvent = function () {
+            $scope.eventModal.hide();
+        }
+    })
 ;
+
