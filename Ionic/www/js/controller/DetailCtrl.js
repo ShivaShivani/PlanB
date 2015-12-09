eee7243f86d54109a1d645af68f813c006f0569b
ShivaniGaddala <shivanigaddala@akhilas-mbp.kc.umkc.edu>
2015-12-08 23:11:55 -0600

    Included Friends Invite UI

--- Ionic/www/js/controller/DetailCtrl.js
+++ Ionic/www/js/controller/DetailCtrl.js
@@ -1,9 +1,44 @@
 /**
  * Created by shivanigaddala on 11/11/15.
  */
-myapp.controller('DetailCtrl', function ($scope, $state, $stateParams, Events) {
+myapp.controller('DetailCtrl', function ($scope, $state, $stateParams, Events, $ionicModal, Friends, FriendsService) {
     console.log($stateParams.eventId);
     $scope.event = Events.get($stateParams.eventId);
+    $scope.invitees = [];
 
     console.log($scope.event);
+
+    //Create and load the Modal
+    $ionicModal.fromTemplateUrl('views/inviteFriend.html', function (modal) {
+        $scope.inviteModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+
+    $scope.presentInviteFriends = function() {
+        $scope.inviteModal.show()
+    };
+
+    $scope.closeModal = function() {
+        $scope.inviteModal.hide()
+    };
+//    Friends service for showing in the modal
+    $scope.friends = [];
+
+    FriendsService.all(function (response) {
+
+        console.log(response);
+        Friends.copy(response);
+        $scope.friends = response;
+
+        console.log("Friends downloaded");
+    }, function (response) {
+        prompt(response);
+    });
+    
+    $scope.invite = function (friend) {
+        console.log(friend);
+        $scope.invitees.push(friend);
+    }
 });
